ba0832d2bfef05e79a524e755747c22993e19d89
Paulo Melo <pjsm.it@proton.me>
2025-12-20 19:00:09 +0000

    STYLE: wrap long print statements according to PEP8.

--- chapter_02/examples/vampire_test.py
+++ chapter_02/examples/vampire_test.py
@@ -1,6 +1,7 @@
-print("Hello possible fiend!\nWelcome to M.O.R.G.A.N.A - Monitoring of Rare Gothic and Nocturnal Anomalies")
+print(
+    "Hello possible fiend!\nWelcome to M.O.R.G.A.N.A - Monitoring "
+    "of Rare Gothic and Nocturnal Anomalies")
 
-# Input do utilizador
 fiend_name = input("Enter your name, possible fiend: ").strip().title()
 while True:
     try:
@@ -11,7 +12,6 @@ while True:
 
 print(f"\nHello, {fiend_name}.")
 
-# Lógica de deteção
 if fiend_age < 12:
     print("Nothing suspicious here. You are a human child.")
 elif fiend_age < 90:
@@ -20,19 +20,25 @@ elif fiend_age <= 150:
     print("This is looking kinda sus!")
 else:
     print("For sure a vampire!\nSelect your extermination method!")
-    print("1 - Stake to the heart\n2 - Sunlight\n3 - Cocktail spiked with holy water")
+    print(
+        "1 - Stake to the heart\n2 - Sunlight\n3 - "
+        "Cocktail spiked with holy water")
 
-    # Escolha do método
     while True:
-        extermination_method = input("Please, select an option (1-3): ").strip()
+        extermination_method = input(
+            "Please, select an option (1-3): ").strip()
         if extermination_method in ["1", "2", "3"]:
             break
         else:
             print("Invalid option. Please select 1, 2, or 3.")
 
     if extermination_method in ["1", "2"]:
-        print("Please walk to the red 'X' on the floor next to you and wait for extermination!")
+        print(
+            "Please walk to the red 'X' on the floor next to you "
+            "and wait for extermination!")
     elif extermination_method == "3":
-        print("Please wait for our mixologist robot to bring you your beverage!")
+        print(
+            "Please wait for our mixologist robot"
+            " to bring you your beverage!")
 
     print("Thank you for your cooperation!")
