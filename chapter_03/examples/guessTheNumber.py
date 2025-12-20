916c8182e84d603d81f2e70500d4d1d17a8f0f67
Paulo Melo <pjsm.it@proton.me>
2025-12-20 22:03:16 +0000

    CREATE: example file from book.

---
+++ chapter_03/examples/guessTheNumber.py
@@ -0,0 +1,23 @@
+import random
+
+secretNumber = random.randint(1, 20)
+print('I am thinking of a number between 1 and 20.')
+
+for guessesTaken in range(1, 7):
+    print('Take a guess.')
+    guess = int(input())
+
+    if guess < secretNumber:
+        print('Your guess is too low.')
+    elif guess > secretNumber:
+        print('Your guess is too high.')
+    else:
+        break
+
+if guess == secretNumber:
+    print(
+        'Good job! Your guessed my nymber in '
+        + str(guessesTaken) + ' guesses!'
+        )
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber))
