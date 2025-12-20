514cb2610b21d4f9fa78426a2bb4fbc54fe38051
Paulo Melo <pjsm.it@proton.me>
2025-12-20 20:11:38 +0000

    CREATE: exercise file.

---
+++ chapter_02/exercises/exercise07.py
@@ -0,0 +1,5 @@
+# A condition is an expression that evaluates to either True or False.
+# It represents a logical test that determines whether a
+# specific block of code should run.
+# Conditions are used wherever a program needs to make decisions or control
+# the flow of execution based on data or user input.
