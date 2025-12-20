6db61743b96a838bf526ff4c88be568ad8991aeb
Paulo Melo <pjsm.it@proton.me>
2025-12-20 14:53:17 +0000

    CREATE exercise 1 file.

---
+++ part_01/question01.py
@@ -0,0 +1,7 @@
+# * -> multiplication operator
+# 'hello' -> string - not an operator
+# - 88.8 -> negative float - not an operator
+# - -> subtraction operator
+# / -> division operator
+# + -> sum operator
+# 5 -> integer - not an operator
