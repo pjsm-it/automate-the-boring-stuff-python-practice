7d7fa52b95f9e364be38bafefb3bc64036009496
Paulo Melo <pjsm.it@proton.me>
2025-12-20 15:05:33 +0000

    CREATE exercise 6 file.

---
+++ part_01/exercise06.py
@@ -0,0 +1,5 @@
+# 20
+# The int value 20 is assigned to the variable bacon.
+# The expression 20 + 1 is evaluated and results in 21.
+# The value stored in bacon remains unchanged because there is no new
+# assignment.
