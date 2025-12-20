c450625031653ea724c0831b07a58fdd5aabf085
Paulo Melo <pjsm.it@proton.me>
2025-12-20 19:35:51 +0000

    CREATE: exercise file.

---
+++ chapter_02/exercises/exercise04.py
@@ -0,0 +1,24 @@
+# (5 > 4) and (3 == 5)
+# True and False
+# False
+
+# not (5 > 4)
+# not True
+# False
+
+# (5 > 4) or (3 == 5)
+# True or False
+# True
+
+# not ((5 > 4) or (3 == 5))
+# not (True or False)
+# not True
+# False
+
+# (True and True) and (True == False)
+# True and False
+# False
+
+# (not False) or (not True)
+# True or False
+# True
