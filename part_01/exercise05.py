8919cda56aa553963611034d07c640712aeae4ad
Paulo Melo <pjsm.it@proton.me>
2025-12-20 15:01:44 +0000

    FIX typo in exercise 5 file.

--- part_01/exercise05.py
+++ part_01/exercise05.py
@@ -1,2 +1,4 @@
-# An expression uses values and operators, evaluates them, and produces a result.
-# An assignment statement stores a value in memory under a specific variable name.
+# An expression uses values and operators, evaluates them, and produces a
+# result.
+# An assignment statement stores a value in memory under a specific variable
+# name.
