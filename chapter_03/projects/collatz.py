423ec93590cdc18395cb4de12264d7161902d79e
Paulo Melo <pjsm.it@proton.me>
2025-12-22 15:43:54 +0000

    CREATE a collatz sequence small project.

---
+++ chapter_03/projects/collatz.py
@@ -0,0 +1,21 @@
+def collatz(number):
+    if number % 2 == 0:
+        result = number // 2
+    else:
+        result = 3 * number + 1
+    print(result)
+    return result
+
+
+def run_collatz_sequence():
+    number = int(input("Enter an integer: "))
+    while number != 1:
+        number = collatz(number)
+
+
+def main():
+    run_collatz_sequence()
+
+
+if __name__ == "__main__":
+    main()
