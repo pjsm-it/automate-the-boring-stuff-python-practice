25ee020e417ebd11f077cc09b581096a9d1b5bb8
Paulo Melo <pjsm.it@proton.me>
2025-12-22 20:54:01 +0000

    CREATE exercise file.

---
+++ chapter_04/exercises/exercise17.py
@@ -0,0 +1,5 @@
+# copy.copy() creates a shallow copy - creates a new outer object,
+#  but the elements inside are still references/pointers
+#  to the original objects.
+# copy.deepcopy() creates a deep copy - creates a independent
+#  copy that does not affect the original.
