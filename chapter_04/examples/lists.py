08b73f80f600124704767f88245942f42c51f69b
Paulo Melo <pjsm.it@proton.me>
2025-12-22 15:58:45 +0000

    UPDATE example file.

--- chapter_04/examples/lists.py
+++ chapter_04/examples/lists.py
@@ -1,3 +1,27 @@
 spam = ['cat', 'bat', 'rat', 'elephant']
 
 print(f"The {spam[-1]} is afraid of the {spam[-2]}.")
+
+print()
+print(spam[0:4])
+print(spam[1:3])
+print(spam[0:-1])
+print(spam[:2])
+print(spam[1:])
+print(spam[:])
+
+print()
+print(len(spam))
+
+print()
+print(spam[1])
+spam[1] = 'aardvark'
+print(spam)
+
+print()
+spam = spam + ['1', '2', '3']
+print(spam)
+
+print()
+del spam[-1]
+print(spam)
