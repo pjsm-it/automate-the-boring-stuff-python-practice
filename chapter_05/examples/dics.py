315877c7f21aeef497c3344d9a30402ff55b90aa
Paulo Melo <pjsm.it@proton.me>
2025-12-22 21:54:43 +0000

    CREATE example file.

---
+++ chapter_05/examples/dics.py
@@ -0,0 +1,21 @@
+myCat = {
+    'size': 'fat',
+    'color': 'gray',
+    'disposition': 'loud'
+}
+
+print(myCat['size'])
+
+# Dictionaries are unordered
+print()
+eggs = {
+    'name': 'Zophie',
+    'species': 'cat',
+    'age': '8'
+}
+ham = {
+    'species': 'cat',
+    'age': '8',
+    'name': 'Zophie'
+}
+print(eggs == ham)
